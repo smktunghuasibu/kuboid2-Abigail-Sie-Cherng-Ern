98aac7f78501389083d67cfb8ba562bc1d9ee254
Abigail-Sie-Cherng-Ern <163367476+Abigail-Sie-Cherng-Ern@users.noreply.github.com>
2024-08-06 05:52:19 +0800

    Update kuboid.py make mistake

--- workfile/kuboid.py
+++ workfile/kuboid.py
@@ -2,7 +2,7 @@ def kuboid():
     a = float(input("Masukkan panjang: "))
     b = float(input("Masukkan lebar: "))
     c = float(input("Masukkan tinggi: "))
-    isipadu_kuboid = a * b * c
+    isipadu_kuboid = a * b / c
     print(f"Isipadu kuboid = {isipadu_kuboid}")
 
 # JANGAN ubah kod di bawah
